7266e8c72997e91b545d6d17766915e54df89303
jiwony <jiwony3758@gmail.com>
2023-11-20 15:24:27 +0900

    Study: ADC 인터럽트 방식, 이동평균, 10초, 100초, 1000초 필터링, 각 평균마다 최대 최소를 제외한 후 평균내기

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,6 +18,9 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include <limits.h>
+#include <float.h>
+
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -32,6 +35,8 @@
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 
+#define MEASURE_CYCLE 99;
+
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -43,6 +48,39 @@
 ADC_HandleTypeDef hadc1;
 
 /* USER CODE BEGIN PV */
+
+int measure_count = 0;
+
+int interval_time = 0;
+int analog_value;
+int SAMPLE_SIZE = 10;
+int sample_index = 0;
+float moving_avg_sample[10] = { 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000 };
+
+float moving_avg_sample_sum;
+float moving_avg_result_value;
+
+int current_time = 0;
+int pre_time = 0;
+int measure_pre_time = 0;
+
+float bit_voltage;
+float voltage;
+
+float avg_10;
+float avg_10_data[10];
+float avg_100;
+float avg_100_data[10];
+float avg_1000;
+float avg_1000_data[10];
+
+int array_10_index;
+int array_100_index;
+int array_1000_index;
+float full_sum_avg10;
+float full_sum_avg100;
+
+float* arr_address;
 
 /* USER CODE END PV */
 
@@ -58,6 +96,234 @@ static void MX_MEMORYMAP_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+
+float Average_Calculate10() {
+
+  float min = FLT_MAX;
+  float max = FLT_MIN;
+  int min_index = 0;
+  int max_index = 0;
+
+  float sum = 0.0;
+
+  for(int i = 0; i < 10; i++) {
+    if(min > avg_10_data[i]) {
+      min = avg_10_data[i];
+      min_index = i;
+    }
+
+    if(max < avg_10_data[i]) {
+      max = avg_10_data[i];
+      max_index = i;
+    }
+
+  }
+  for(int j = 0; j < 10; j++) {
+    full_sum_avg10 = full_sum_avg10 + avg_10_data[j];
+    if(max_index == j || min_index == j){
+      continue;
+    } else {
+      sum = sum + avg_10_data[j];
+    }
+
+  }
+  return  sum / 8;
+}
+
+float Average_Calculate100() {
+
+  float min = FLT_MAX;
+  float max = FLT_MIN;
+  int min_index = 0;
+  int max_index = 0;
+
+  float sum = 0.0;
+
+  for(int i = 0; i < 10; i++) {
+    if(min > avg_100_data[i]) {
+      min = avg_100_data[i];
+      min_index = i;
+    }
+
+    if(max < avg_100_data[i]) {
+      max = avg_100_data[i];
+      max_index = i;
+    }
+
+  }
+  for(int i = 0; i < 10; i++) {
+    full_sum_avg100 = full_sum_avg100 + avg_100_data[i];
+    if(max_index == i || min_index == i){
+      continue;
+    } else {
+      sum = sum + avg_100_data[i];
+    }
+
+  }
+  return  sum / 8;
+}
+
+float Average_Calculate1000() {
+
+  float min = FLT_MAX;
+  float max = FLT_MIN;
+  int min_index = 0;
+  int max_index = 0;
+
+  float sum = 0.0;
+
+  for(int i = 0; i < 10; i++) {
+    if(min > avg_1000_data[i]) {
+      min = avg_1000_data[i];
+      min_index = i;
+    }
+
+    if(max < avg_1000_data[i]) {
+      max = avg_1000_data[i];
+      max_index = i;
+    }
+
+  }
+  for(int i = 0; i < 10; i++) {
+    if(max_index == i || min_index == i){
+      continue;
+    } else {
+      sum = sum + avg_1000_data[i];
+    }
+  }
+  return  sum / 8;
+}
+
+void Value_Allocate(float value) {
+  int array_length = sizeof(avg_10_data) / sizeof(float);
+  array_10_index = measure_count % array_length;
+
+  avg_10_data[array_10_index] = value;
+
+  if(measure_count > 0 && array_10_index == 9) {
+    array_100_index = measure_count / 10 % array_length;
+
+    avg_10 = Average_Calculate10();
+    avg_100_data[array_100_index] = full_sum_avg10 / 10;
+    full_sum_avg10 = 0.0;
+  }
+
+  if(measure_count > 10 && array_100_index == 9) {
+    array_1000_index = measure_count / 100 % array_length;
+
+    avg_100 = Average_Calculate100();
+    avg_1000_data[array_1000_index] = full_sum_avg100 / 10;
+    full_sum_avg100 = 0.0;
+  }
+
+  if(measure_count > 10 && array_1000_index == 9) {
+    avg_1000 = Average_Calculate1000();
+  }
+}
+
+
+
+/*
+void Average_10_Calculate(float value) {
+  int array_length = sizeof(avg_10_data) / sizeof(float);
+  array_10_index = measure_count % array_length;
+  avg_10_data[array_10_index] = value;
+
+  if(measure_count > 0 && measure_count % array_length == 9) {
+
+    float data_sum = 0.0;
+    for(int i = 0; i < array_length; i++) {
+      data_sum = data_sum + avg_10_data[i];
+      avg_10_data[i] = 0;
+    }
+
+
+
+    avg_10 = data_sum / array_length;
+  }
+}
+
+void Average_100_Calculate(float value) {
+  int array_length = sizeof(avg_100_data) / sizeof(float);
+  array_100_index = measure_count % array_length;
+  avg_100_data[array_100_index] = value;
+
+
+  if(measure_count > 10 && measure_count % array_length == 9) {
+    float data_sum = 0.0;
+    for(int i = 0; i < array_length; i++) {
+      data_sum = data_sum + avg_100_data[i];
+      avg_100_data[i] = 0;
+    }
+
+    avg_100 = data_sum / array_length;
+  }
+}
+
+void Average_1000_Calculate(float value) {
+  int array_length = sizeof(avg_1000_data) / sizeof(float);
+  array_1000_index = measure_count % array_length;
+  avg_1000_data[array_1000_index] = value;
+
+  if(measure_count > 10 && measure_count % array_length == 9) {
+    float data_sum = 0.0;
+    for(int i = 0; i < array_length; i++) {
+      data_sum = data_sum + avg_1000_data[i];
+      avg_1000_data[i] = 0;
+    }
+
+    avg_1000 = data_sum / array_length;
+  }
+}
+*/
+void Moving_Sample_Sum_Calculate() {
+  moving_avg_sample_sum = 0;
+
+  for(int i = 0; i < SAMPLE_SIZE; i++){
+    moving_avg_sample_sum += moving_avg_sample[i];
+    if(i < SAMPLE_SIZE -1 && moving_avg_sample[i + 1] == 5000) {
+      break;
+    }
+  }
+}
+
+void Handle_Moving_Sample_New_Data(float value) {
+
+  if(moving_avg_sample[SAMPLE_SIZE - 1] != 5000){
+
+    for(int i = 1; i < SAMPLE_SIZE; i++) {
+      moving_avg_sample[i - 1] = moving_avg_sample[i];
+    }
+  }
+  moving_avg_sample[sample_index] = value;
+}
+
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc) {
+
+    analog_value = HAL_ADC_GetValue(hadc);
+    bit_voltage = 3.3 / 4096;
+    voltage = bit_voltage * analog_value;
+    Handle_Moving_Sample_New_Data(voltage);
+    Moving_Sample_Sum_Calculate();
+    moving_avg_result_value = moving_avg_sample_sum / (sample_index + 1);
+
+
+    if(current_time - measure_pre_time > 999) {
+
+      // Average_10_Calculate(moving_avg_result_value);
+      // Average_100_Calculate(moving_avg_result_value);
+      // Average_1000_Calculate(moving_avg_result_value);
+      Value_Allocate(moving_avg_result_value);
+      measure_count++;
+      measure_pre_time = current_time;
+    }
+
+
+    if(sample_index < SAMPLE_SIZE - 1) {
+      sample_index++;
+    }
+
+}
 
 /* USER CODE END 0 */
 
@@ -96,12 +362,21 @@ int main(void)
   MX_MEMORYMAP_Init();
   /* USER CODE BEGIN 2 */
 
+  HAL_ADC_Start_IT(&hadc1);
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+    current_time = HAL_GetTick();
+
+    if(current_time - pre_time > 99) {
+      HAL_ADC_Start_IT(&hadc1);
+      interval_time = current_time - pre_time;
+      pre_time = current_time;
+    }
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -205,7 +480,7 @@ static void MX_ADC1_Init(void)
   /** Common config
   */
   hadc1.Instance = ADC1;
-  hadc1.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV2;
   hadc1.Init.Resolution = ADC_RESOLUTION_12B;
   hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
   hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
@@ -228,7 +503,7 @@ static void MX_ADC1_Init(void)
   */
   sConfig.Channel = ADC_CHANNEL_1;
   sConfig.Rank = ADC_REGULAR_RANK_1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_6CYCLES_5;
+  sConfig.SamplingTime = ADC_SAMPLETIME_2CYCLES_5;
   sConfig.SingleDiff = ADC_SINGLE_ENDED;
   sConfig.OffsetNumber = ADC_OFFSET_NONE;
   sConfig.Offset = 0;
